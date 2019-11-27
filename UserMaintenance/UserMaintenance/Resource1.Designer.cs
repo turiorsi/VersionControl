0c898331b51b878813931d2c552e69b9ef851587
Orsi Turi <t.orsi83@gmail.com>
2019-11-27 10:29:35 +0100

    commit

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UserMaintenance.Entities {
+namespace UserMaintenance {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace UserMaintenance.Entities {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UserMaintenance.Entities.Resource1", typeof(Resource1).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UserMaintenance.Resource1", typeof(Resource1).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
