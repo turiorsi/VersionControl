6c75f81c7c6261a243fa9293339a57ce0bf8813f
Orsi Turi <t.orsi83@gmail.com>
2019-11-27 10:57:19 +0100

    commit5

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.txtFirstName = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFajl = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -86,11 +87,22 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // btnFajl
+            // 
+            this.btnFajl.Location = new System.Drawing.Point(295, 168);
+            this.btnFajl.Name = "btnFajl";
+            this.btnFajl.Size = new System.Drawing.Size(166, 23);
+            this.btnFajl.TabIndex = 6;
+            this.btnFajl.Text = "button1";
+            this.btnFajl.UseVisualStyleBackColor = true;
+            this.btnFajl.Click += new System.EventHandler(this.btnFajl_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFajl);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.txtLastName);
@@ -112,6 +124,7 @@
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.TextBox txtFirstName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFajl;
     }
 }
 
